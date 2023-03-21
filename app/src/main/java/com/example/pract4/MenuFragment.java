9daf7c439deb99475d5e01ffc18b09424eb33115
Daniil <firefly.daniil@gmail.com>
2023-03-21 20:51:31 +0300

    Fourth Commit

--- app/src/main/java/com/example/pract4/MenuFragment.java
+++ app/src/main/java/com/example/pract4/MenuFragment.java
@@ -21,7 +21,6 @@ public class MenuFragment extends Fragment {
         binding = FragmentMenuBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
-    casuchasihc
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
